2f4a40dffdb3d112fece9fc3290ca2d11a487be1
MichaelCMD <michael.j.crowleymcdevitt>
2018-09-20 15:03:47 +0100

    Completed Ex2.java

--- ProblemSet1/src/Ex2.java
+++ ProblemSet1/src/Ex2.java
@@ -6,7 +6,6 @@ public class Ex2 {
 
         int yards;
         final int CONVERSION_FACTOR = 12*3;
-        String list="";
 
         JTextArea textArea = new JTextArea(10,40);
         Font f = new Font("monospaced",Font.PLAIN,12);
@@ -16,10 +15,6 @@ public class Ex2 {
         for(yards=1;yards <= 10;yards++)
         {
             textArea.append(String.format("%-10d%d\n",yards,yards*CONVERSION_FACTOR));
-            //list += String.format("%-10d%d\n",yards,yards*CONVERSION_FACTOR);
-
-
-            //yards++;
         }
 
         JOptionPane.showMessageDialog(null,textArea,"Yards to inches table",JOptionPane.INFORMATION_MESSAGE);
