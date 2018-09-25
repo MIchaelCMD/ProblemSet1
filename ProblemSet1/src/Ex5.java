bd5b4c38458b4e6b620ec85ce7f8885dcde5c6c4
t00200043 <michael.j.crowleymcdevitt@students.ittralee.ie>
2018-09-25 17:33:36 +0100

    Attempted Ex5.java :/

--- ProblemSet1/src/Ex5.java
+++ ProblemSet1/src/Ex5.java
@@ -1,7 +1,21 @@
+import javax.swing.*;
+
 public class Ex5 {
     public static void main(String[] args) {
 
         int b = 6, c = -16, a = 1;
+        double x1, x2;
+        String x;
 
+
+        x1 = (-b + (Math.sqrt((b * b)) - (4 * a * c)) / (2 * a));
+
+        x2 = (-b - Math.sqrt((b * b) - (4 * a * c)) / (2 * a));
+
+        x = x1 + "\n " + x2;
+
+        //JOptionPane.showMessageDialog(null,x1 + "\n" + x2);
+
+        JOptionPane.showMessageDialog(null,"Values: " + x);
     }
 }
