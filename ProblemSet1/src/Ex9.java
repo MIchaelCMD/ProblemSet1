d7b0edece4fb67b65e18232f42e7ee22da5a9f1c
t00200043 <michael.j.crowleymcdevitt@students.ittralee.ie>
2018-10-02 17:35:39 +0100

    Completed Ex9.java

--- ProblemSet1/src/Ex9.java
+++ ProblemSet1/src/Ex9.java
@@ -6,11 +6,11 @@ public class Ex9 {
         String[] weightsAS = new String[10];
         String weightOutput = "";
         double[] weights = new double[10];
-        int under250 = 0, over400 = 0, minValue = weights[0];
+        double minValue = 1000000, totalWeight = 0, average = 0;
+        int under250 = 0, over400 = 0;
 
         for(int i = 0; i < weightsAS.length; i++)
         {
-
             weightsAS[i]= JOptionPane.showInputDialog("Enter Value For Array["+(i + 1)+"].");
 
             weights[i] = Double.parseDouble(weightsAS[i]);
@@ -27,12 +27,20 @@ public class Ex9 {
                 over400++;
             }
 
-            if(weights[i] < )
+            if(weights[i] < minValue)
+            {
+                minValue = weights[i];
+            }
+
+            totalWeight += weights[i];
         }
+
+        average = (totalWeight/weights.length);
 
         JOptionPane.showMessageDialog(null,"Weights: " + weightOutput + "\n" +
                 "Total animals below 250KG: " + under250 + "\n" +
                 "Percentage over 400KG: " + (over400*10) + "%\n" +
-                "","Animal Weight Stats",JOptionPane.INFORMATION_MESSAGE);
+                "The lightest animal is: " + minValue + "KG\n" +
+                "Average Weight: " + average + "KG.","Animal Weight Stats",JOptionPane.INFORMATION_MESSAGE);
     }
 }
